dcd062233fad4d336bbca0764a648c576f776606
Anh Nguyen <anhnt.si@gmail.com>
2021-04-30 23:28:51 +0700

    Hard code trading fee

--- binance_trade_bot/binance_api_manager.py
+++ binance_trade_bot/binance_api_manager.py
@@ -41,6 +41,8 @@ class BinanceAPIManager:
         return self.binance_client.get_bnb_burn_spot_margin()["spotBNBBurn"]
 
     def get_fee(self, origin_coin: Coin, target_coin: Coin, selling: bool):
+        return 0.001
+        
         base_fee = self.get_trade_fees()[origin_coin + target_coin]
         if not self.get_using_bnb_for_fees():
             return base_fee
